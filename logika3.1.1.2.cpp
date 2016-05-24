8e22d6d7ca7f42d69a7e4cc83b4f74ea216c87a1
Nabywaniec <Nabywaniec@users.noreply.github.com>
2016-05-24 17:55:10 +0200

    Update logika3.1.1.2.cpp

--- logika3.1.1.2.cpp
+++ logika3.1.1.2.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int function(int a[6])
 {
+	
+	if(a[0]==0 || a[1]==0 || a[2]==0 || a[3]==0 ||  a[4]==0 || a[5]==0){
+		cout << "Niepoprawne argumenty";
+		cout << endl;
+		return 0;
+	}	
 	if(a[0]<=a[1] or a[2]==0 or ( a[3]%a[1]!=1  and  a[4]%a[0]==0 and a[5]%a[3]!=2) )
 		return ((a[1]-a[0]) + a[2]%a[1] + a[0]%a[1]+ a[3]%a[4]);
 		else return (a[0]-a[1]);
